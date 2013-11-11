3a934c81c14b47fe1687c37e42e3bbf5c3fe981f
Ken <insidepower2@gmail.com>
2013-11-11 11:36:38 +0800

    added new knf

--- Text.java
+++
@@ -1,6 +0,0 @@
-[ EditText ]
-
-EditText et_passwd = (EditText) findViewById(R.id.txt_password);
-String passwd = et_passwd.getText().toString();
-if (passwd.equals("03012007")) {
-[ ------------------------------------------------------------------ ]
